56627bbd01df472f1b507c3d2ae742920a6eec17
mahzze <zanzinzzo@gmail.com>
2022-11-26 18:51:22 -0300

    fix reload infinito

--- src/components/Login/sair.tsx
+++ src/components/Login/sair.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 export default function Sair() {
+
+  if (sessionStorage.length > 0) {
     // deletar o cookie também
     sessionStorage.clear();
-    
-    // reload infinito
     window.location.reload();
+  }
 
-    return(
-        <h1>Deslogado!</h1>
-    )
+  return (
+    <h1>Deslogado!</h1>
+  )
 }
